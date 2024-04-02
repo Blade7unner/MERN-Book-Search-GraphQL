ec925cd46ba9d4573c7949b5cfe0f2947d583931
Benoit <fbenoit2077@gmail.com>
2024-04-02 14:51:09 -0400

    added mutations and queries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Create Apollo client instance
+const client = new ApolloClient({
+  uri: 'your-graphql-endpoint', // Replace 'your-graphql-endpoint' with your actual GraphQL endpoint
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
