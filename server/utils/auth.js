01a2b627db4be421c2aed7cb2935d9e1bb4d84cc
Benoit <fbenoit2077@gmail.com>
2024-04-02 13:36:05 -0400

    auth.js token extract, error logging, token verification

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,40 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
+  // Middleware function for authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // Allow token to be sent via req.query, headers, or cookies
+    let token = req.query.token || req.headers.authorization || req.cookies.token;
 
-    // ["Bearer", "<tokenvalue>"]
+    // Check if token exists and format if sent in headers
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return res.status(401).json({ message: 'You are not authorized to access this resource.' });
     }
 
-    // verify token and get user data out of it
+    // Verify token and extract user data from it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(token, secret);
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (error) {
+      console.error('Invalid token:', error);
+      return res.status(401).json({ message: 'Invalid token.' });
     }
 
-    // send to next endpoint
+    // Proceed to the next middleware
     next();
   },
+
+  // Function to sign JWT token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
